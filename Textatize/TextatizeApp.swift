850ecdeeabcf8a21695af6e95edb38aeedfb0b07
Tornelius <tkbjr01@icloud.com>
2023-05-15 10:47:50 -0400

    Initial Commit

---
+++ Textatize/TextatizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextatizeApp.swift
+//  Textatize
+//
+//  Created by Tornelius Broadwater, Jr on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TextatizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
