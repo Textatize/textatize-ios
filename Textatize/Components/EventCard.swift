20893da7579bf71c212d6dacbed1411b90afd7dd
Tornelius <tkbjr01@icloud.com>
2023-05-25 10:28:41 -0400

    cleanup

--- Textatize/Components/EventCard.swift
+++ Textatize/Components/EventCard.swift
@@ -119,43 +119,6 @@ struct EventCard: View {
     }
 }
 
-struct NewEventCard: View {
-    
-    //@Binding var newEventPressed: Bool
-    
-    var body: some View {
-        
-        Button {
-            print("New Event Pressed")
-            
-            //            withAnimation {
-            //                newEventPressed = true
-            //            }
-        } label: {
-            VStack(spacing: 5) {
-                ZStack {
-                    Circle()
-                        .fill(.white)
-                        .frame(width: 40)
-                    AppImages.EventCard.plus
-                }
-                Text("New Event")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                
-            }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background {
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(AppColors.Onboarding.bottomColor)
-                    .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
-            }
-            
-        }
-    }
-}
 
 struct EventCard_Previews: PreviewProvider {
     static var previews: some View {
