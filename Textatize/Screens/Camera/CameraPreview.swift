c0e9a0d008fa891cefe5d2616ee796b6768f2989
Tornelius <tkbjr01@icloud.com>
2023-06-14 13:32:17 -0400

    Capture Fixes

--- Textatize/Screens/Camera/CameraPreview.swift
+++ Textatize/Screens/Camera/CameraPreview.swift
@@ -8,27 +8,75 @@
 import SwiftUI
 import AVFoundation
 
-struct CameraPreview: UIViewRepresentable {
+class CameraPreviewView: UIView {
+    private var captureSession: AVCaptureSession
     
-    @ObservedObject var camera: CameraModel
-    
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
-        
-        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
-        
-        camera.preview.frame = view.frame
-        
-        camera.preview.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(camera.preview)
-                
-        camera.session.startRunning()
-        
-        return view
+    init(session: AVCaptureSession) {
+        self.captureSession = session
+        super.init(frame: .zero)
     }
     
-    func updateUIView(_ uiView: UIView, context: Context) {
-        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        if nil != self.superview {
+            self.videoPreviewLayer.session = self.captureSession
+            self.videoPreviewLayer.videoGravity = .resizeAspectFill
+            //Setting the videoOrientation if needed
+            //self.videoPreviewLayer.connection?.videoOrientation = .landscapeRight
+        } else {
+            self.videoPreviewLayer.session = nil
+            self.videoPreviewLayer.removeFromSuperlayer()
+        }
+    }
 }
+
+struct CameraPreviewHolder: UIViewRepresentable {
+    var captureSession: AVCaptureSession
+    
+    func makeUIView(context: UIViewRepresentableContext<CameraPreviewHolder>) -> CameraPreviewView {
+        CameraPreviewView(session: captureSession)
+    }
+    
+    func updateUIView(_ uiView: CameraPreviewView, context: UIViewRepresentableContext<CameraPreviewHolder>) {
+    }
+    
+    typealias UIViewType = CameraPreviewView
+}
+
+//struct CameraPreview: UIViewRepresentable {
+//    
+//    @ObservedObject var camera: CameraModel
+//    
+//    func makeUIView(context: Context) -> UIView {
+//        let view = UIView(frame: UIScreen.main.bounds)
+//        
+//        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+//        
+//        camera.preview.frame = view.frame
+//        
+//        camera.preview.videoGravity = .resizeAspectFill
+//        view.layer.addSublayer(camera.preview)
+//                
+//        camera.session.startRunning()
+//        
+//        return view
+//    }
+//    
+//    func updateUIView(_ uiView: UIView, context: Context) {
+//        
+//    }
+//    
+//}
--- Textatize/Screens/Camera/CameraView.swift
+++ Textatize/Screens/Camera/CameraView.swift
@@ -9,12 +9,11 @@ import SwiftUI
 import Combine
 import Kingfisher
 
-
 struct CameraView: View {
     @Environment(\.dismiss) var dismiss
     var event: Event? = nil
     var frame: Frame? = nil
-    @StateObject private var camera = CameraModel()
+    @StateObject private var camera = CameraManager.shared
     @State private var continuePressed = false
     @State private var countDown = 5
     @State var timer: Timer.TimerPublisher = Timer.publish(every: 1, on: .main, in: .common)
@@ -22,7 +21,7 @@ struct CameraView: View {
     
     var body: some View {
         ZStack {
-            CameraPreview(camera: camera)
+            CameraPreviewHolder(captureSession: camera.session)
                 .ignoresSafeArea()
             
             if camera.isTaken {
@@ -90,12 +89,11 @@ struct CameraView: View {
                 ZStack {
                     Color.black.opacity(0.75)
                         .ignoresSafeArea()
-
                     
                     SharePhotoView(eventID: event?.unique_id ?? "NO ID", showView: $continuePressed, imageData: camera.retrieveImage()!, image: camera.processedPhoto)
                         .padding()
                 }
-            
+                
             }
         }
         .onReceive(timer, perform: { time in
@@ -115,11 +113,13 @@ struct CameraView: View {
             let _ = camera.processPhotos(frame: frame)
         })
         .onDisappear {
+            camera.isTaken = false
             cancelTimer()
+            camera.session.stopRunning()
         }
         .toolbar(.hidden, for: .tabBar)
+        
     }
-    
     private func instantiateTimer() {
         self.timer = Timer.publish(every: 1, on: .main, in: .common)
         self.connectedTimer = self.timer.connect()
@@ -142,7 +142,132 @@ struct CameraView: View {
         self.instantiateTimer()
         return
     }
+    
 }
+    
+//    var body: some View {
+//        ZStack {
+//            CameraPreview(camera: camera)
+//                .ignoresSafeArea()
+//            
+//            if camera.isTaken {
+//                ZStack {
+//                    
+//                    Color.black.opacity(camera.photoReady ? 0.95 : 0)
+//                        .ignoresSafeArea()
+//                    
+//                    Image(uiImage: camera.processedPhoto ?? UIImage(systemName: "photo")!)
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                        .padding()
+//                    
+//                    VStack {
+//                        
+//                        Spacer()
+//                        HStack {
+//                            Button {
+//                                withAnimation {
+//                                    continuePressed = true
+//                                }
+//                            } label: {
+//                                Text("Continue")
+//                                    .foregroundColor(.black)
+//                                    .fontWeight(.semibold)
+//                                    .padding(.vertical, 10)
+//                                    .padding(.horizontal, 20)
+//                                    .background(Color.white)
+//                                    .clipShape(Capsule())
+//                            }
+//                            .padding(.leading)
+//                            .padding()
+//                            
+//                            Spacer()
+//                            
+//                            Button {
+//                                print("Retake Button Pressed")
+//                                camera.reTake()
+//                                restartTimer()
+//                            } label: {
+//                                Text("Retake")
+//                                    .foregroundColor(.black)
+//                                    .fontWeight(.semibold)
+//                                    .padding(.vertical, 10)
+//                                    .padding(.horizontal, 20)
+//                                    .background(Color.white)
+//                                    .clipShape(Capsule())
+//                            }
+//                            .padding(.trailing)
+//                            .padding()
+//                        }
+//                    }
+//                }
+//                .opacity(camera.photoReady ? 1 : 0)
+//            } else {
+//                Circle()
+//                    .strokeBorder(.red, lineWidth: 5)
+//                    .frame(width: UIScreen.main.bounds.width * 0.27, height: UIScreen.main.bounds.width * 0.27)
+//                Text("\(countDown)")
+//                    .font(.system(size: 50, weight: .bold))
+//                    .foregroundColor(.red)
+//            }
+//            
+//            if continuePressed {
+//                ZStack {
+//                    Color.black.opacity(0.75)
+//                        .ignoresSafeArea()
+//
+//                    
+//                    SharePhotoView(eventID: event?.unique_id ?? "NO ID", showView: $continuePressed, imageData: camera.retrieveImage()!, image: camera.processedPhoto)
+//                        .padding()
+//                }
+//            
+//            }
+//        }
+//        .onReceive(timer, perform: { time in
+//            if countDown > 0 {
+//                countDown -= 1
+//            } else {
+//                print("Countdown hit zero, pic taken")
+//                camera.takePic()
+//                cancelTimer()
+//            }
+//        })
+//        .onAppear {
+//            camera.check()
+//            instantiateTimer()
+//        }
+//        .onChange(of: camera.picData, perform: { value in
+//            let _ = camera.processPhotos(frame: frame)
+//        })
+//        .onDisappear {
+//            cancelTimer()
+//        }
+//        .toolbar(.hidden, for: .tabBar)
+//    }
+//    
+//    private func instantiateTimer() {
+//        self.timer = Timer.publish(every: 1, on: .main, in: .common)
+//        self.connectedTimer = self.timer.connect()
+//        return
+//    }
+//    
+//    private func cancelTimer() {
+//        self.connectedTimer?.cancel()
+//        return
+//    }
+//    
+//    private func resetCounter() {
+//        self.countDown = 5
+//        return
+//    }
+//    
+//    private func restartTimer() {
+//        self.countDown = 5
+//        self.cancelTimer()
+//        self.instantiateTimer()
+//        return
+//    }
+
 
 //struct CameraView_Previews: PreviewProvider {
 //    static var previews: some View {
